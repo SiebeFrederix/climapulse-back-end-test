c82ba3925847ad3a97e422f3c6de45ad4712c954
Siebe Frederix <frederixsiebe@gmail.com>
2026-01-16 11:29:27 +0100

    Bug fix using F expression

--- refrigerant/management/commands/withdraw.py
+++ refrigerant/management/commands/withdraw.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db.models import F
 from ...models import Vessel
 import threading
 
@@ -7,6 +8,7 @@ class Command(BaseCommand):
     help = "Simulate condition when withdrawing refrigerant from a vessel."
 
     def handle(self, *args, **kwargs):
+        # This will generate a new Vessel instance everytime the migrate command is ran
         Vessel.objects.create(name="Test Vessel", content=50.0)
         self.stdout.write("Simulating condition...")
         self.run_simulation()
@@ -16,15 +18,16 @@ class Command(BaseCommand):
 
         def user1():
             barrier.wait()
-            vessel = Vessel.objects.get(id=1)
-            vessel.content -= 10.0
-            vessel.save()
+            # Only the first created Vessel is used in every withdraw prompt, hence id=1
+            vessel = Vessel.objects.filter(id=1)
+            # F expression performs an actual SQL operation on the database
+            # It does not load the data into a local python variable first
+            vessel.update(content=F("content") - 10)
 
         def user2():
             barrier.wait()
-            vessel = Vessel.objects.get(id=1)
-            vessel.content -= 10.0
-            vessel.save()
+            vessel = Vessel.objects.filter(id=1)
+            vessel.update(content=F("content") - 10)
 
         t1 = threading.Thread(target=user1)
         t2 = threading.Thread(target=user2)
